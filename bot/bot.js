0376e802058070e72016c39ed0cb17966a7fc978
Austin King <austink@porch.com>
2015-06-04 15:54:23 -0700

    Use hipchatter. Added lightning png

--- bot/bot.js
+++ bot/bot.js
@@ -1,6 +1,16 @@
-
 var fs = require('fs');
 var path = require('path');
 
+var Hipchatter = require('hipchatter');
+
 var config = JSON.parse(
 	fs.readFileSync(path.join(__dirname, '../config/dev.json')));
+
+var hc = new Hipchatter(config.hipchat_bot_token);
+hc.send_private_message(1073704, {
+  message: "So I hear you're Blocked (blockaid)",
+  notify: false,
+  message_format: 'text'
+}, function(err, unknown, resCode) {
+  console.log(err, unknown, resCode);
+});
